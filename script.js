7353a5142c6d9d8185be873934a47506e78834ea
Simona Cartolina <simonacartolina@hotmail.com>
2023-05-16 13:34:17 +0200

    final push js cicle for

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-console.log('Serie di numeri')
+
 
 const ulElement= document.querySelector('ul');
 
-for (let i=0; 0<10; i= i + 1){
+for (let i=0; i<10; i= i + 1){
 
     const li= document.createElement('li');
 
     li.append('This is number: ' + i);
     
-    ulElement.append(li);
+    ulElement.appendChild(li);
 }
